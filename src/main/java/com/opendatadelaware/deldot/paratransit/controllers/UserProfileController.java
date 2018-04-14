f96772848edc5c4d54cff8c5b1bbecc8ec4151f5
Ryan <rgross@udel.edu>
2018-04-13 23:29:27 -0400

    Fixed Eligibility PDF, added skeletons for Controllers

---
+++ src/main/java/com/opendatadelaware/deldot/paratransit/controllers/UserProfileController.java
@@ -0,0 +1,24 @@
+package com.opendatadelaware.deldot.paratransit.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserProfileController {
+
+    @PostMapping(value = "/api/registration")
+    public ResponseEntity<String> registerApplicant(@RequestBody String name) {
+        return ResponseEntity.ok("Registered!");
+    }
+
+    @PostMapping(value ="/api/login")
+    public ResponseEntity<String> login(@RequestParam String userName, @RequestParam String password) {
+        return ResponseEntity.ok("Logged in");
+    }
+
+    @GetMapping(value = "/api")
+    public String helloWorld() {
+        return "derp";
+    }
+
+}
